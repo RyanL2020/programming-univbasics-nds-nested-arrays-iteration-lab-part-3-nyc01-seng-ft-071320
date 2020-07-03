c89dda905eadc2b986697c9728625b785b775acc
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-03 00:36:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,13 +5,13 @@
 #]
 
 def join_nested_strings(src)
-  phrase = " ".to_s
+  phrase = " "
   row_index = 0
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
-         phrase << src[row_index][element_index]
+         phrase += src[row_index][element_index] + ' '
       end
       element_index += 1
     end
